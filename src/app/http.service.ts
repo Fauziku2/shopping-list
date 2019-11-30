7609c0530884211abb59cb68f0bb75ffeb3c6117
Fauzi Mohd Ghouse <m.fauzi_87@hotmail.com>
2019-11-30 10:54:52 +0800

    create http.service.ts and add environment configuration

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class HttpService implements HttpInterceptor {
@@ -10,9 +11,8 @@ export class HttpService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let r: HttpRequest<any>;
     r = req.clone({
-      url: 'http://localhost:3000' + req.url
+      url: environment.api + req.url
     });
-
     return next.handle(r);
   }
 }
